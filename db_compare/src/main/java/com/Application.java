fe1dcc55dbdc7500618ea3a32d9b708e800f2d31
cxxyjsj <809349617@qq.com>
2016-05-28 21:48:10 +0800

    添加运行时参数支持

--- db_compare/src/main/java/com/Application.java
+++ db_compare/src/main/java/com/Application.java
@@ -1,20 +1,37 @@
 package com;
 
+import java.util.Map;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 
+import com.bean.ArgsHolder;
+
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer implements EmbeddedServletContainerCustomizer {
 
     @Override
 	public void customize(ConfigurableEmbeddedServletContainer container) {
-		container.setPort(80);
+    	Map<String, Object> config = ArgsHolder.configs;
+    	if(config.get("port") != null){
+    		container.setPort(Integer.valueOf(config.get("port").toString()));
+    	}else{
+    		container.setPort(80);
+    	}
 	}
 
 	public static void main(String[] args) {
+		if(args != null && args.length > 0){
+			for(int i=0;i<args.length;i++){
+				if(args[i].startsWith("--") && i < args.length - 1){
+					String key = args[i].replace("--", "");
+					ArgsHolder.configs.put(key, args[i+1]);
+				}
+			}
+		}
         SpringApplication.run(Application.class, args);
     }
 }
---
+++ db_compare/src/main/java/com/bean/ArgsHolder.java
@@ -0,0 +1,12 @@
+package com.bean;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author cxxyjsj
+ * @date 2016年5月28日 下午9:40:31
+ */
+public class ArgsHolder {
+	public static final Map<String,Object> configs = new HashMap<>();
+}
